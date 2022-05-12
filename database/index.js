3e21cd5331b1fc24e74a4ccef20521f35dde3641
faizrafii <m.faiz8@yahoo.com>
2022-05-12 19:57:28 +0700

    prepare for heroku

--- database/index.js
+++ database/index.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const dv = require('../app/config.js');
 
-const password = dv.PASSWORD;
-const connAtlas = 'mongodb://faiz1:' + password + '@cluster0-shard-00-00.bd5hs.mongodb.net:27017,cluster0-shard-00-01.' +
-'bd5hs.mongodb.net:27017,cluster0-shard-00-02.bd5hs.mongodb.net:27017/cluster0?ssl=true&' +
-'replicaSet=atlas-10erh7-shard-0&authSource=admin&retryWrites=true&w=majority';
+// const password = dv.PASSWORD;
+// const connAtlas = 'mongodb://faiz1:' + password + '@cluster0-shard-00-00.bd5hs.mongodb.net:27017,cluster0-shard-00-01.' +
+// 'bd5hs.mongodb.net:27017,cluster0-shard-00-02.bd5hs.mongodb.net:27017/cluster0?ssl=true&' +
+// 'replicaSet=atlas-10erh7-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 const pass = '123'
 const onlineDB = 'eduwork-1'
@@ -12,11 +12,10 @@ const connAtlas2 = `mongodb://faiz1:${pass}@cluster0-shard-00-00.bd5hs.mongodb.n
   '27017,cluster0-shard-00-01.bd5hs.mongodb.net:27017,cluster0-shard-00-02.bd5hs.mongodb.net:27017' +
   `/${onlineDB}?ssl=true&replicaSet=atlas-10erh7-shard-0&authSource=admin&retryWrites=true&w=majority`
 
-const connLocal = 'mongodb://mainuser:123@localhost:27017/mongoosedb?authSource=admin';
 const local = 'mongodb://localhost:27017/eduwork-1';
 // console.log(connAtlas);
 
-mongoose.connect(local);
+mongoose.connect(connAtlas2);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error test atlas: '));
